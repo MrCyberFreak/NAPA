a0d453f2107e8864452eb8ca5c6034bcca09245d
cyberdabadoo <cyberdabadoo@gmail.com>
2026-06-11 15:07:40 -0600

    onboard(13711): activate Wreckroom Sunday in the scrape registry
    Two-line onboarding diff: scrape flag + rollout tripwire.

--- src/config.py
+++ src/config.py
@@ -54,7 +54,7 @@ DIVISIONS: dict[int, Division] = {
         Division(13986, "Zoosters Laggers", "Tuesday", "LC", scrape=True),
         Division(13937, "Pharaoh's", "Wednesday", "LC", scrape=True),
         Division(13881, "Broomfield Westminster Laggers", "Monday", "LC", scrape=True),
-        Division(13711, "Wreckroom Sunday", "Sunday", "LC"),
+        Division(13711, "Wreckroom Sunday", "Sunday", "LC", scrape=True),
         Division(13299, "Piazzas Tuesday", "Tuesday", "LC"),
         Division(13205, "Greeley", "Monday", "LC"),
         Division(13744, "DP Broomfield Westminster LC", "Friday", "LC"),
